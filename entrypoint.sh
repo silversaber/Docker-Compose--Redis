d3c011be6e487bd94026888d2f2e2c4c138907f2
aglide100 <35767154+aglide100@users.noreply.github.com>
2020-09-30 23:01:13 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,13 @@
 
 if [[ -n "$USERNAME" ]] && [[ -n "$USERPWD" ]]
 then
-	htpasswd -bc /etc/nginx/user1.htpasswd  $USERNAME $USERPWD
-	htpasswd -bc /etc/nginx/user2.htpasswd  $USERNAME2 $USERPWD2
-	echo Done.
+	if [[ -n "$USERNAME2" ]] && [[ -n "$USERPWD2" ]]
+	then
+		htpasswd -bc /etc/nginx/user1.htpasswd  $USERNAME $USERPWD $USERNAME2 $USERPWD2
+		echo Done.
+	else 
+		htpasswd -bc /etc/nginx/user2.htpasswd  $USERNAME $USERPWD
+		echo Done.
 else
     echo no auth file
 fi
